c3531fdcc3272f5f0bf33b6c08ec7eba7913db0f
tagvayaset <104499717+tagvayaset@users.noreply.github.com>
2022-05-01 12:12:04 +0600

    Delete javaFX_player directory

--- javaFX_player/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.javafx_player {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.media;
-
-
-    opens com.example.javafx_player to javafx.fxml;
-    exports com.example.javafx_player;
-}
